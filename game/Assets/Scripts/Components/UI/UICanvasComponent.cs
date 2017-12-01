8014fb9ccb376460d7e2e79cf3cafe44dffe5914
dorofiykolya <dorofiy.kolya@gmail.com>
2017-12-01 20:10:50 +0200

    reload game

---
+++ game/Assets/Scripts/Components/UI/UICanvasComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DevGate
+{
+    class UICanvasComponent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (EventSystem.current == null)
+            {
+                Instantiate(Resources.Load("Prefabs/EventSystem"));
+            }
+        }
+    }
+}
--- game/Assets/Scripts/GameSceneManager.cs
+++ game/Assets/Scripts/GameSceneManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using Utils;
 
 namespace DevGate
@@ -12,14 +14,21 @@ namespace DevGate
         public Lifetime.Definition Load(Scenes sceneName)
         {
             var def = Lifetime.Define(GameContext.Lifetime);
-            GameContext.StartCoroutine(def.Lifetime, LoadScene(sceneName));
+            var async = SceneManager.LoadSceneAsync(sceneName.ToString(), LoadSceneMode.Additive);
+            async.completed += operation =>
+            {
+                var scene = SceneManager.GetSceneByName(sceneName.ToString());
+                SceneManager.SetActiveScene(scene);
+                def.Lifetime.AddAction(() =>
+                {
+                    foreach (var gameObject in scene.GetRootGameObjects())
+                    {
+                        GameObject.Destroy(gameObject);
+                    }
+                    SceneManager.UnloadSceneAsync(scene);
+                });
+            };
             return def;
-        }
-
-        private IEnumerator LoadScene(Scenes sceneName)
-        {
-            var async = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName.ToString());
-            yield return async;
         }
     }
 }
