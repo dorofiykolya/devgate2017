8c3f02f94e1661d10da335b124b9d249c9ebee8d
dorofiykolya <dorofiy.kolya@gmail.com>
2017-12-01 22:54:48 +0200

    camera post process

---
+++ game/Assets/Scripts/Components/Camera/CameraPostEffectComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DevGate
+{
+    public class CameraPostEffectComponent : MonoBehaviour
+    {
+        public Material Material;
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            Material.SetTexture("_MainTex", src);
+            Graphics.Blit(src, dest, Material);
+        }
+    }
+}
