07669e2e754e1012a8db267164c08d0028ab7f35
dorofiykolya <dorofiy.kolya@gmail.com>
2017-12-02 00:57:25 +0200

    boat

---
+++ game/Assets/Scripts/Components/Boat/BoatFanComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DevGate
+{
+    [ExecuteInEditMode]
+    public class BoatFanComponent : MonoBehaviour
+    {
+        public Transform Transform;
+        public float Speed = 1f;
+
+        private void Update()
+        {
+            Transform.Rotate(new Vector3(0, 0, 1), Speed * Time.deltaTime);
+        }
+    }
+}
