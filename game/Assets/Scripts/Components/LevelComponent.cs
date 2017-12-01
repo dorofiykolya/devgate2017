be391aadf4c72e7c2e7cfee76b99a5a564f14b13
dorofiykolya <dorofiy.kolya@gmail.com>
2017-12-01 21:18:33 +0200

    Settings

--- game/Assets/Scripts/Components/LevelComponent.cs
+++ game/Assets/Scripts/Components/LevelComponent.cs
@@ -10,16 +10,11 @@ namespace DevGate
     {
         public Camera Camera;
         public Animator StartLevelAnimator;
-        [HideInInspector]
-        public InputControllerComponent InputController;
-        public GameHudComponent HudComponent;
+        public LevelSettingsScriptableObject Settings;
 
         public void StartLevel()
         {
             StartLevelAnimator.SetTrigger("Start");
-            InputController = new InputControllerComponent();
-            InputController.Init();
-            HudComponent.Init(this);
         }
     }
 }
