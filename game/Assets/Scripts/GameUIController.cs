0d49aca80c93744621e22f209356fb80800cb0c6
Maxim Odinets <odinets@seriouscake.net>
2017-12-01 17:41:17 +0200

    Input and Camera controllers

---
+++ game/Assets/Scripts/GameUIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Slider _powerSlider;
+
+    [SerializeField]
+    private InputController _inputController;
+
+    
+    void Update()
+    {
+        _powerSlider.value = _inputController.CurrentPower / 100f;
+    }
+}
---
+++ game/Assets/Scripts/InputController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class InputController : MonoBehaviour
+{
+
+    private float _currentPower = 0;
+    private float _horizontalPosition = 0;
+
+    public float CurrentPower { get { return _currentPower; } }
+    public float HorizontalPosition { get { return _horizontalPosition; } }
+
+    private Vector2 _startTouchPosition = Vector2.zero;
+
+    private Signal _onShoot;
+
+    private void Awake()
+    {
+        Input.simulateMouseWithTouches = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(Input.touchCount);
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.touches[0];
+            if (touch.phase == TouchPhase.Began)
+            {
+                _startTouchPosition = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                _currentPower = _startTouchPosition.y - touch.position.y;
+                _horizontalPosition = touch.position.x - _startTouchPosition.x;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                _onShoot.Fire();
+                _currentPower = 0;
+                _horizontalPosition = 0;
+                _startTouchPosition = Vector2.zero;
+            }
+        }
+    }
+
+    public void SubscribeOnShoot(Lifetime lifetime,Action callback)
+    {
+        _onShoot.Subscribe(lifetime, callback);
+    }
+}
