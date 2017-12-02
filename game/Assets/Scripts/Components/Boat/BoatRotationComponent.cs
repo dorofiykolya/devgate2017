5030d3b72cb9b7cd9f2e2a886fd75597944898d2
Maxim Odinets <maximodinets@gmail.com>
2017-12-02 07:15:06 +0200

    reset rotation after shoot

--- game/Assets/Scripts/Components/Boat/BoatRotationComponent.cs
+++ game/Assets/Scripts/Components/Boat/BoatRotationComponent.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BoatRotationComponent : MonoBehaviour
 {
+    private bool resetRotation = false;
+    private Vector3 targetRotation;
+    private float speed = 6f;
 
     void Start()
     {
         GameContext.LevelController.Current.InputController.SubscribeOnHorizontalCHange(GameContext.Lifetime, SetHorizontalPosition);
+        GameContext.LevelController.Current.InputController.SubscribeOnShoot(GameContext.Lifetime,  ResetRotation);
     }
 
     private void SetHorizontalPosition(float value)
@@ -15,5 +19,21 @@ public class BoatRotationComponent : MonoBehaviour
         Vector3 r = transform.localEulerAngles;
         r.y = value;
         transform.localEulerAngles = r;
+    }
+
+    private void ResetRotation()
+    {
+        targetRotation = transform.localEulerAngles;
+        targetRotation.y = 0;
+        resetRotation = true;
+    }
+
+    private void Update()
+    {
+        if (!resetRotation) return;
+        var r = transform.localEulerAngles;
+        r.y = Mathf.LerpAngle(r.y, 0, speed * Time.deltaTime);
+        transform.localEulerAngles = r;
+        resetRotation = Mathf.Abs(r.y) > 0.02f;
     }
 }
