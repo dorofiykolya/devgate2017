77046a97ae39c023c523afb7b757b054d05ac454
Maxim Odinets <maximodinets@gmail.com>
2017-12-02 04:40:05 +0200

    camera controller //TODO disable animator

--- game/Assets/Scripts/CameraController.cs
+++ game/Assets/Scripts/CameraController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	
+	void Start ()
+    {
+        GameContext.LevelController.Current.InputController.SubscribeOnHorizontalCHange(GameContext.Lifetime, RotateCamera);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	void RotateCamera(float value)
+    {
+        Vector3 r = transform.localEulerAngles;
+        r.y = value * 0.2f;
+        transform.localEulerAngles = r;
+    }
 }
