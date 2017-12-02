f45ff5030d98ff8a5df6965a5dde19104d01b45b
Maxim Odinets <maximodinets@gmail.com>
2017-12-02 12:12:14 +0200

    fix blink popup

--- game/Assets/Scripts/Levels/LevelState.cs
+++ game/Assets/Scripts/Levels/LevelState.cs
@@ -20,6 +20,8 @@ namespace DevGate
 
         private Signal<LevelState> _onStateChanged;
 
+        private bool _gameOver = false;
+
         public void Init(Lifetime lifetime)
         {
             _onStateChanged = new Signal<LevelState>(lifetime);
@@ -33,27 +35,32 @@ namespace DevGate
 
         public void UpdateLife(int delta)
         {
+            if (_gameOver) return;
             _life += delta;
             _onStateChanged.Fire(this);
             if (_life <= 0)
             {
                 _life = 0;
+                _gameOver = true;
                 LoosePopup.ShowPopup();
             }
         }
 
         public void UpdateScore(int delta)
         {
+            if (_gameOver) return;
             _score += delta;
             _onStateChanged.Fire(this);
             if (_score >= GameContext.LevelController.Current.Settings.ScoreForFirstLevel)
             {
+                _gameOver = true;
                 WinPopup.ShowPopup();
             }
         }
 
         public void ResetLevelState()
         {
+            _gameOver = false;
             _life = 5;
             _score = 0;
             _onStateChanged.Fire(this);
